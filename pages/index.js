597c616871cb934c0ea056982e608e2af84d3808
Sudarshan Pathak <sushan.pth@gmail.com>
2023-04-28 11:20:52 +0545

    Pre-rendering and Data Fetching
    https://nextjs.org/learn/basics/data-fetching

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+/*
+  Static Generation is the pre-rendering method that generates the HTML at build time. 
+  The pre-rendered HTML is then reused on each request.
+  Using Static Generation (getStaticProps())
+*/
+export async function getStaticProps(){
+  const allPostsData = getSortedPostsData()
+
+  return {
+    props: {
+      allPostsData,
+    }
+  }
+}
+/* 
+  To use Server-side Rendering, you need to export getServerSideProps instead of getStaticProps from your page.
+  Because getServerSideProps is called at request time, its parameter (context) contains request specific parameters.
+*/
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       // props for your component
+//     },
+//   };
+// }
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -19,6 +46,22 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+
       </section>
     </Layout>
   );
